885752e583418b92a72b7826e0de3918cbc3e214
Jun Ying <jun.y@ubuntoo.com>
2023-05-10 17:04:34 -0400

    Remove extra log.

--- library/Vanilla/EmbeddedContent/Factories/UbuntooEmbedFactory.php
+++ library/Vanilla/EmbeddedContent/Factories/UbuntooEmbedFactory.php
@@ -212,17 +212,11 @@ class UbuntooEmbedFactory extends AbstractEmbedFactory {
      * @throws \Exception If the scrape fails.
      */
     private function queryGraphQL(string $contentUrl, string $operationName, string $query): object {
-        ErrorLogger::warning(
-            "CONTENT URL: $contentUrl",
-            ["post", "embed"]
-        );
+
         $scheme = parse_url($contentUrl, PHP_URL_SCHEME);
         $host = parse_url($contentUrl, PHP_URL_HOST);
         $apiHost = $scheme . '://' . $host;
-        ErrorLogger::warning(
-            "API HOST: $apiHost",
-            ["post", "embed"]
-        );
+
         $graphqlEndpoint = $apiHost .'/api/graphql';
 
         $client = new \GuzzleHttp\Client();
