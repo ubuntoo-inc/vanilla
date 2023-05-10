dc64d6dc4ec61fdca46f5bb8d1aaa1ddd9b2eb77
Jun Ying <jun.y@ubuntoo.com>
2023-05-10 00:15:50 -0400

    Add copy of fastcgi.conf.

--- static/start-server.sh
+++ static/start-server.sh
@@ -4,6 +4,7 @@ echo "starting php"
 service php8.0-fpm start
 sleep 10
 
+cp /ebs/nginx/conf/fastcgi.conf.tpl /etc/nginx/fastcgi.conf
 #service nginx start
 echo "starting nginx"
 service nginx start
