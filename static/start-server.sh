a5d28410e9fd5b958e4124599244956fe02942f4
Jun Ying <jun.y@ubuntoo.com>
2023-05-10 12:49:05 -0400

    Reduce file ops in build step as efs is very slow.

--- static/start-server.sh
+++ static/start-server.sh
@@ -4,6 +4,10 @@ echo "starting php"
 service php8.0-fpm start
 sleep 10
 
+chown www-data:www-data -R /ebs
+chmod 777 /ebs/vanilla/conf
+chmod 777 /ebs/vanilla/uploads
+chmod 777 /ebs/vanilla/cache
 cp /ebs/nginx/conf/fastcgi.conf.tpl /etc/nginx/fastcgi.conf
 cp /ebs/nginx/conf/vanilla-web.conf /etc/nginx/sites-available
 ln -s /etc/nginx/sites-available/vanilla-web.conf /etc/nginx/sites-enabled
